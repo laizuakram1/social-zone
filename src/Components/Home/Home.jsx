85aeb074da16ce49ba9bd112908c246b50d060b8
laizu akram <laizuakram98@gmail.com>
2023-08-21 23:14:03 +0600

    setup tailwind css and daisy ui

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h3 className="text-3xl font-bold underline">welcome to social zone</h3>
+        </div>
+    );
+};
+
+export default Home;
